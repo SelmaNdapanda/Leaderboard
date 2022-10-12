8177b91ae9ef30e3721b7009bf858036c9bb44c3
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-10-13 00:25:21 +0200

    Update score.js

--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,6 +1,7 @@
 export default class Scores {
-  constructor(name, score) {
-    this.name = name;
-    this.score = score;
+  constructor(user_name, points) {
+    user_name = this.user_name;
+    // eslint-disable-next-line no-unused-vars
+    points = this.points;
   }
 }
